3ad13ac2daf0793d04dab9d6bfbc9e7e242048a2
Lee Millward <lee.millward@gmail.com>
2013-08-30 19:53:12 +0100

    Handle lexing of escaped identifiers.

---
+++ tests/tests/errors/30034-1.vb
@@ -0,0 +1,10 @@
+'#compiler-message:30034-1.vb(7:24) : error BC30034: Bracketed identifier is missing closing ']'.
+'#exit-code:1
+
+Module Test
+
+    Public Sub Main()
+        Dim [Identifier As String
+    end Sub
+    
+End Module
---
+++ tests/tests/errors/30037-1.vb
@@ -0,0 +1,12 @@
+'#compiler-message:30037-1.vb(8:13) : error BC30037: Character is not valid.
+'#exit-code:1
+
+Module Test
+
+    Public Sub Main()
+
+        Dim ] as string
+        
+    End Sub
+    
+End Module
